dceace16f3513718b3f06c9430d45db0a9d77af7
Gomathy <gomathy2511@gmail.com>
2023-05-17 16:28:53 +0530

    cors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ db();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     methods: ['GET', 'POST','DELETE','PUT']
    
 }
